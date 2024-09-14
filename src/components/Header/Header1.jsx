a3572285a378cba9225733a7bc2c2a53c4c14802
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-09-14 17:19:10 +0530

    generated url added

--- src/components/Header/Header1.jsx
+++ src/components/Header/Header1.jsx
@@ -17,7 +17,7 @@ export const Header = () => {
         <div className="container">
           <div className="st-main-header-in">
             <div className="st-main-header-left">
-              <Link className="st-site-branding" to='/' id="hero"><img src="src/assets/Atman-logo.png" alt="Atman Law" /></Link>
+              <Link className="st-site-branding" to='/' id="hero"><img src="https://res.cloudinary.com/dgtsyf1vu/image/upload/v1726313720/Atman-logo_fpia1r.png" alt="Atman Law" /></Link>
             </div>
             <div className="st-main-header-right">
               <div className="st-nav">
